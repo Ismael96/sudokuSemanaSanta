5eff0a453d5adf6386c2a4c9683706952b806d14
ICC-ICZ <ICC-ICZ@ICC>
2017-04-16 18:52:25 +0200

    Menú del juego. Al crearlo me he dado cuenta de que me faltan metodos. Será arreglado en la siguiente version.

--- src/sudokusemanasanta/SudokuSemanaSanta.java
+++ src/sudokusemanasanta/SudokuSemanaSanta.java
@@ -5,6 +5,8 @@
  */
 package sudokusemanasanta;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICC-ICZ
@@ -15,7 +17,68 @@ public class SudokuSemanaSanta {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      {
+
+        ClaseSudoku sudoku = new ClaseSudoku();
+       
+        Scanner teclado = new Scanner(System.in);
+        
+        int numero2 = 0;
+
+        do {
+
+            System.out.println("1. Iniciar/Reiniciar sudoku\n" + "2. Realizar movimiento\n"+ "3. Vaciar casilla \n"+ "4. Mostrar sudoku\n"+ "5.Reiniciar\n"+ "6. Salir");
+
+            numero2 = teclado.nextInt();
+
+            if (numero2 == 1) {
+                sudoku.CrearyEmpezar();
+            }
+
+            if (numero2 == 2) {
+                System.out.println("*********************************************************************");
+
+                System.out.println("Indica la fila: ");
+                int fila = teclado.nextInt();
+                System.out.println("*********************************************************************");
+                System.out.println("Indica la columna:");
+                int columna = teclado.nextInt();
+                System.out.println("Indica el número:");
+                int numero = teclado.nextInt();
+
+                sudoku.cambiar(fila, columna, numero);
+                
+                System.out.println(sudoku.toString());
+            }
+
+            if (numero2 == 3) {
+                System.out.println("Dime las coordenadas de la casilla que quieres vaciar:");
+                System.out.println("Fila: ");
+                int fila = teclado.nextInt();
+                System.out.println("Columna:");
+                int columna = teclado.nextInt();
+
+                sudoku.eliminar(fila, columna);
+            }
+
+            if (numero2 == 4) {
+                System.out.println(sudoku.toString());
+            }
+            if (numero2 == 5) {
+                sudoku.CrearyEmpezar();
+                
+
+            }
+            if (numero2 == 6) {
+                
+                System.out.println("Has salido");
+
+            }
+        } while (numero2 <= 6);
+
     }
-    
 }
+
+}
+    
+
